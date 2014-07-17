fe1ec53ddc7dd739cc28f244d3de517ac63ae75e
hollybrenan <holly@ustwo-ldn-holly-brenan-mbp13-2.local>
2014-07-17 17:47:49 +0100

    less changes

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,27 @@
 //= require_tree .
 $(document).ready(function(){
-	$('.startButton').click(function(){
+	$('#buttonOne').click(function(){
+		$('#cardOne').slideUp('slow');
+		return false;
+	});
+
+	$('#buttonTwo').click(function(){
 		$('#cardTwo').slideUp('slow');
+		return false;
+	});
+
+	$('#buttonThree').click(function(){
+		$('#cardThree').slideUp('slow');
+		return false;
+	});
+
+	$('#buttonFour').click(function(){
+		$('#cardFour').slideUp('slow');
+		return false;
+	});
+
+	$('#buttonFive').click(function(){
+		$('#cardFive').slideUp('slow');
+		return false;
 	});
 });
