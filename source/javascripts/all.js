0e94fb78ca0667be2d236b9a377cec7499db331b
hollybrenan <holly@ustwo-ldn-holly-brenan-mbp13-2.local>
2014-07-23 12:17:37 +0100

    changes

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,31 +1,53 @@
 //= require_tree .
+
+
+
 $(document).ready(function(){
-	$('#buttonOne').click(function(){
-		$('#cardOne').slideUp('slow');
-		return false;
-	});
 
-	$('#buttonTwo').click(function(){
-		$('#cardTwo').slideUp('slow');
-		return false;
-	});
+$("#buttonOne").click(function () {
+      $('#cardOne').slideUp('slow');
+      return false;
+});
 
-	$('#previousTwo').click(function(){
-		$('#cardTwo').slideDown('slow');
-	});
 
-	$('#buttonThree').click(function(){
-		$('#cardThree').slideUp('slow');
-		return false;
-	});
+$('#buttonTwo').click(function(){
+$('#cardTwo').slideUp('slow');
+return false;
+});
 
-	$('#buttonFour').click(function(){
-		$('#cardFour').slideUp('slow');
-		return false;
-	});
+$("#backTwo").click(function() {
+	$("#cardOne").slideDown();
+	return false;
+});
 
-	$('#buttonFive').click(function(){
-		$('#cardFive').slideUp('slow');
-		return false;
-	});
+$('#buttonThree').click(function(){
+$('#cardThree').slideUp('slow');
+return false;
+});
+
+$("#backThree").click(function() {
+	$("#cardTwo").slideDown();
+	return false;
+});
+
+$('#buttonFour').click(function(){
+$('#cardFour').slideUp('slow');
+return false;
+});
+
+$("#backFour").click(function() {
+	$("#cardThree").slideDown();
+	return false;
+});
+
+$('#buttonFive').click(function(){
+$('#cardFive').slideUp('slow');
+return false;
+});
+
+$("#backFive").click(function() {
+	$("#cardFour").slideDown();
+	return false;
+});
+
 });
